63288829144137dc07ad18686ebcba39ad4d22e8
neob91 <neob91@gmail.com>
2019-07-14 18:37:26 +0200

    First version

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const storesOpenSundays = [
+    '2019-07-28',
+    '2019-08-25',
+    '2019-09-29',
+    '2019-10-27',
+    '2019-11-24',
+    '2019-12-15',
+    '2019-12-22',
+    '2019-12-29'
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+    const [currentSunday, isTodaySunday] = getCurrentSunday();
+    const isCurrentSundayOpen = storesOpenSundays.indexOf(getDateString(currentSunday)) !== -1;
+    const futureOpenSundays = getFutureSundaysWithOpenStores();
+    const currentElem = document.querySelector('.current');
+
+    currentElem.querySelector('.text-1').innerText = (
+        isTodaySunday ?
+            'Czy dzisiaj jest niedziela handlowa?' :
+            'Czy najbliższa niedziela jest handlowa?'
+    );
+
+    currentElem.querySelector('.text-2').innerText = isCurrentSundayOpen ? 'TAK! :-)' : 'NIE ;-(';
+
+    currentElem.querySelector('.text-3').innerText = (
+        `${getDateDisplay(currentSunday)} ${isCurrentSundayOpen ? '' : 'nie '}jest niedzielą handlową.`
+    );
+
+    currentElem.classList.toggle('stores-closed', !isCurrentSundayOpen);
+    document.querySelector('.future > .text-1').innerText = (
+        isCurrentSundayOpen ?
+            'Kolejne niedziele handlowe' :
+            'Nadchodzące niedziele handlowe'
+    );
+    document.querySelector('.future > .text-2').innerHTML = futureOpenSundays.map(getDateDisplay).slice(0, 4).join('<br />');
+});
+
+const getCurrentSunday = () => {
+    const d = new Date();
+    const weekday = d.getDay();
+    const isTodaySunday = weekday === 0;
+
+    if (!isTodaySunday) {
+        d.setDate(d.getDate() + 7 - weekday);
+    }
+
+    return [d, isTodaySunday];
+};
+
+const getFutureSundaysWithOpenStores = () => {
+    const sunday = getDateString(getCurrentSunday()[0]);
+    return storesOpenSundays.filter(d => d > sunday).map(d => new Date(d));
+};
+
+const getTodayDateString = () => getDateString(new Date());
+const getDateString = d => d.toISOString().split('T')[0];
+const getDateDisplay = d => d.toLocaleDateString('pl-PL');
